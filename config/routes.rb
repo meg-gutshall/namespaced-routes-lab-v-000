4c5427a5336f67dab3a5e927c95ad41b659333f2
Meg Gutshall <meghan.gutshall@gmail.com>
2019-05-17 15:07:09 -0400

    Adjust nested routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :songs
   resources :artists do
-    resources :songs, only: [:index, :show, :new, :edit]
+    resources :songs, only: [:index, :show]
   end
   namespace :admin do
     resources :preferences
