6e7df75d7e7c9a403555a46ff1c385b5578500de
Meg Gutshall <meghan.gutshall@gmail.com>
2019-05-17 12:29:52 -0400

    Draw artists and songs routes and namespace admin/preferences routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists
+  resources :songs
+  namespace :admin do
+    resources :preferences
+  end
 end
