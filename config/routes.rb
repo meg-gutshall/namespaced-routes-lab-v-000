74acdbef5c486b73dafaeee415053af20ce30d74
Meg Gutshall <meghan.gutshall@gmail.com>
2019-05-17 14:48:06 -0400

    Update routes.rb file to reflect nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :artists
   resources :songs
+  resources :artists do
+    resources :songs, only: [:index, :show, :new, :edit]
+  end
   namespace :admin do
     resources :preferences
   end
