3175837e511da10ade0d8fc03a8f0b0484282ae0
Meg Gutshall <meghan.gutshall@gmail.com>
2019-05-21 11:19:18 -0400

    Clear bad code

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,16 +1,12 @@
 class Admin::PreferencesController < ApplicationController
   def index
-    @preference = Preference.create(preference_params)
+    # @preference = Preference.first_or_create(artist_sort_order: "ASC", song_sort_order: "ASC", allow_create_artist: true, allow_create_songs: true)
   end
 
   def update
-    @preference = Preference.find(params[:id])
-    @preference.update(preference_params)
-    redirect_to admin_preferences_path
+    # @preference = Preference.find(params[:id])
+    # @preference.update(preference_params)
+    # redirect_to admin_preferences_path
   end
-
-  private
-    def preference_params
-      params.require(:preference).permit(:allow_create_artists, :allow_create_songs, :artist_sort_order, :song_sort_order)
-    end
+  
 end
