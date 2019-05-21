f5817d8066da3af016479d1c5fe1c75f99e6085c
Meg Gutshall <meghan.gutshall@gmail.com>
2019-05-21 11:37:44 -0400

    Controller updates

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,6 +1,6 @@
 class Admin::PreferencesController < ApplicationController
   def index
-    # @preference = Preference.first_or_create(artist_sort_order: "ASC", song_sort_order: "ASC", allow_create_artist: true, allow_create_songs: true)
+    # @preference = Preference.first_or_create(artist_sort_order: "ASC", song_sort_order: "ASC", allow_create_artists: true, allow_create_songs: true)
   end
 
   def update
